163447602a7de95ad088aea2ec7f4a0fd6924c57
Sebastian Nozzi <sebnozzi@gmail.com>
2013-12-01 00:43:04 +0100

    Change in logic, testing other things - shutdown

--- src/main/scala/clashcode/Main.scala
+++ src/main/scala/clashcode/Main.scala
@@ -2,32 +2,63 @@ package clashcode
 
 import akka.actor._
 import akka.event.Logging
-import scala.concurrent.duration.FiniteDuration
-import java.util.concurrent.TimeUnit
+
+//import scala.concurrent.duration.FiniteDuration
+//import java.util.concurrent.TimeUnit
+
+import scala.collection.mutable
 
 import wordguess.messages._
 
 object Main extends App {
 
+  var system:ActorSystem = _
+  
   class WordGuesser(name: String, gameServer: ActorRef) extends Actor {
     import scala.concurrent.ExecutionContext.Implicits._
 
-    //    context.system.scheduler.schedule(FiniteDuration(10, TimeUnit.SECONDS), FiniteDuration(1, TimeUnit.SECONDS)) {
-    //      introduceYourself
-    //    }
+    //val letterQueue = mutable.Queue[Char]('o', 'l', 'e', 'r', 'h', 'w', 'd')
+    val letterQueue = mutable.Queue[Char]('r', 'y', 'o', 'u', 'p', 'q', 'k')
 
     gameServer ! RequestGame(playerName = name)
 
     def receive = {
-      case status:GameStatus => {
+      case status: GameStatus => {
         println("Got game status: " + status)
+        if (status.remainingTries > 0) {
+          if (!letterQueue.isEmpty) {
+            val letter = letterQueue.dequeue
+            println("Making guess: " + letter)
+            gameServer ! MakeGuess(letter)
+          }
+        }
+      }
+      case NoAvailableGames() => {
+        println("No more games to play - Bye")
+        system.shutdown()
+      }
+      case NotPlayingError() => {
+        println("We are not playing")
+        system.shutdown()
+      }
+      case GameWon(status) => {
+        println("We won the game!")
+        println("The word was: " + status.word.map(_.getOrElse('?')).mkString)
+        gameServer ! MakeGuess('x')
+        //system.shutdown()
+      }
+      case GameLost(status) => {
+        println("We lost the game :-(")
+        println("Remaining word: " + status.word.map(_.getOrElse('_')).mkString)
+        gameServer ! MakeGuess('x')
+        //system.shutdown()
       }
     }
 
   }
 
   override def main(args: Array[String]) {
-    val system = ActorSystem("local")
+    system = ActorSystem("local")
     val gameServer = system.actorFor("akka.tcp://cluster@127.0.0.1:9110/user/main")
     val playerActor = system.actorOf(Props(classOf[WordGuesser], "@playerSen", gameServer), "playerSen")
   }
