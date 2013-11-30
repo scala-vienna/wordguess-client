464168eccda711a0642d3d80368ff9f85f12fd51
Sebastian Nozzi <sebnozzi@gmail.com>
2013-12-01 00:42:45 +0100

    Change to broadcast message

--- src/main/scala/clashcode/wordguess/messages/PlayerBroadcastMessages.scala
+++ src/main/scala/clashcode/wordguess/messages/PlayerBroadcastMessages.scala
@@ -3,4 +3,4 @@ package clashcode.wordguess.messages
 // =============
 // Messages broadcast to all players
 
-case class SuccessfulGuess(letter:Char, before: Seq[Option[Char]], after: Seq[Option[Char]])
+case class SuccessfulGuess(letter:Char, word: Seq[Option[Char]])
