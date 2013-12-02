13e8882caebbd1386c4d32069f4649943a8da24e
Sebastian Nozzi <sebnozzi@gmail.com>
2013-12-02 20:31:52 +0100

    Extracted connection params

--- src/main/scala/clashcode/wordguess/Main.scala
+++ src/main/scala/clashcode/wordguess/Main.scala
@@ -3,10 +3,13 @@ package clashcode.wordguess
 import akka.actor._
 
 object Main extends App {
+  lazy val gameServerPort = 9110
+  lazy val gameServerHost = "127.0.0.1"
   lazy val localPlayerName = "playerSen"
   override def main(args: Array[String]) {
     val system = ActorSystem("local")
-    val gameServer = system.actorFor("akka.tcp://cluster@127.0.0.1:9110/user/main")
+    val gameServer = system.actorFor(
+      s"akka.tcp://cluster@$gameServerHost:$gameServerPort/user/main")
     val playerActor = system.actorOf(Props(
       classOf[WordGuesserClient],
       s"@$localPlayerName",
